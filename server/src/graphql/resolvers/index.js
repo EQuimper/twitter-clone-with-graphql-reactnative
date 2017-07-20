6f5ad39847459729e1130d3968425fe158014287
EQuimper <quimperemanuel@gmail.com>
2017-07-20 19:52:10 -0400

    End ep02

--- server/src/graphql/resolvers/index.js
+++ server/src/graphql/resolvers/index.js
@@ -1,7 +1,16 @@
+import GraphQLDate from 'graphql-date';
+
 import TweetResolvers from './tweet-resolvers';
 
 export default {
+  Date: GraphQLDate,
   Query: {
+    getTweet: TweetResolvers.getTweet,
     getTweets: TweetResolvers.getTweets,
   },
+  Mutation: {
+    createTweet: TweetResolvers.createTweet,
+    updateTweet: TweetResolvers.updateTweet,
+    deleteTweet: TweetResolvers.deleteTweet
+  }
 };
--- server/src/graphql/resolvers/tweet-resolvers.js
+++ server/src/graphql/resolvers/tweet-resolvers.js
@@ -1,5 +1,18 @@
 import Tweet from '../../models/Tweet';
 
 export default {
-  getTweets: () => Tweet.find({}),
+  getTweet: (_, { _id }) => Tweet.findById(_id),
+  getTweets: () => Tweet.find({}).sort({ createdAt: -1 }),
+  createTweet: (_, args) => Tweet.create(args),
+  updateTweet: (_, { _id, ...rest }) => Tweet.findByIdAndUpdate(_id, rest, { new: true }),
+  deleteTweet: async (_, { _id }) => {
+    try {
+      await Tweet.findByIdAndRemove(_id);
+      return {
+        message: 'Delete Success!'
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
 };
