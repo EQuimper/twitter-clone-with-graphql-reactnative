acb5fde75aa15a8d1c34dec1d27a80de90ec50a9
EQuimper <quimperemanuel@gmail.com>
2017-08-12 16:00:54 -0400

    End ep12[

--- mobile/src/actions/user.js
+++ mobile/src/actions/user.js
@@ -1,5 +1,28 @@
+import { AsyncStorage } from 'react-native';
+
 export function login() {
   return {
     type: 'LOGIN'
+  }
+}
+
+export function getUserInfo(info) {
+  return {
+    type: 'GET_USER_INFO',
+    info
+  }
+}
+
+export function logout() {
+  return async (dispatch) => {
+    try {
+      await AsyncStorage.removeItem('@twitteryoutubeclone');
+
+      return dispatch({
+        type: 'LOGOUT'
+      })
+    } catch (error) {
+      throw error;
+    }
   }
 }
--- mobile/src/screens/HomeScreen.js
+++ mobile/src/screens/HomeScreen.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import styled from 'styled-components/native';
-import { graphql } from 'react-apollo';
+import { graphql, compose, withApollo } from 'react-apollo';
 import { ActivityIndicator, FlatList } from 'react-native';
+import { connect } from 'react-redux';
 
 import FeedCard from '../components/FeedCard/FeedCard';
 
+import { getUserInfo } from '../actions/user';
+
 import GET_TWEETS_QUERY from '../graphql/queries/getTweets';
+import ME_QUERY from '../graphql/queries/me';
 
 const Root = styled.View`
   flex: 1;
@@ -13,6 +17,15 @@ const Root = styled.View`
 `;
 
 class HomeScreen extends Component {
+  componentDidMount() {
+    this._getUserInfo();
+  }
+
+  _getUserInfo = async () => {
+    const { data: { me } } = await this.props.client.query({ query: ME_QUERY });
+    this.props.getUserInfo(me);
+  }
+
   _renderItem = ({ item }) => <FeedCard {...item} />
 
   render() {
@@ -37,4 +50,7 @@ class HomeScreen extends Component {
   }
 }
 
-export default graphql(GET_TWEETS_QUERY)(HomeScreen);
+export default withApollo(compose(
+  connect(undefined, { getUserInfo }),
+  graphql(GET_TWEETS_QUERY)
+)(HomeScreen));
